548d1a6e2b8b835bd39f5dd58aafb271c96f94c0
Kristijonas <kristijonas.tarutis@gmail.com>
2023-01-04 21:58:11 +0200

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  eslintConfig: {
+    extends: ['react-app', 'react-app/jest'],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
