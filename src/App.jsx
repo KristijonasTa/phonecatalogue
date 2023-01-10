fe9709ad2e8fc3f807e81a30f89667d518e2d343
Kristijonas <kristijonas.tarutis@gmail.com>
2023-01-10 14:13:37 +0200

    add: routes and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Brand from './pages/Brand';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Phones from './pages/Phones';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div className="page_container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/phones" element={<Phones />} />
+          <Route path="/phones/:brand" element={<Brand />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Phones.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Phones = () => {
+  return <div>Phones</div>;
+};
+
+export default Phones;
