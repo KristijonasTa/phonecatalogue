01ec89d52b403aefc3ad2c684285c3fd0a5ccc18
Kristijonas <kristijonas.tarutis@gmail.com>
2023-02-28 19:16:02 +0200

    displayBrands

--- src/pages/Phones.jsx
+++ src/pages/Phones.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Phones = () => {
-  return <div>Phones</div>;
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    const fetchPhones = async () => {
+      try {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/TGSL-DC/frontend-interview-api/master/brands.json'
+        );
+        const jsonData = await response.json();
+        console.log(jsonData.options);
+        setBrands(jsonData.options);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPhones();
+  }, []);
+
+  return (
+    <ul>
+      {brands.map((phone) => (
+        <li key={phone.displayName}>
+          <Link className="link-to-brands" to={`/phones/${phone.id.toLowerCase()}`}>
+            {phone.displayName}
+            <img src={phone.displayImageUrl} alt="" />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Phones;
